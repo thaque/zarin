d9b07b85bc2f62df5772ac87a58d211e37f072c1
thaque <thaque@thaque.com>
2019-09-10 22:37:05 +0000

    Starting a basic flask app with mysql

--- zarin_app.py
+++ zarin_app.py
@@ -1,27 +1,9 @@
-from flask import Flask, request, make_response
-from processing import process_data
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route("/", methods=["GET", "POST"])
-def file_summer_page():
-    if request.method == "POST":
-        input_file = request.files["input_file"]
-        input_data = input_file.stream.read().decode("utf-8")
-        output_data = process_data(input_data)
-        response = make_response(output_data)
-        response.headers["Content-Disposition"] = "attachment; filename=result.csv"
-        return response
+@app.route("/")
+def index():
+    return render_template("main_page.html")
 
-    return '''
-        <html>
-            <body>
-                <p>Select the file you want to sum up:</p>
-                <form method="post" action="." enctype="multipart/form-data">
-                    <p><input type="file" name="input_file" /></p>
-                    <p><input type="submit" value="Process the file" /></p>
-                </form>
-            </body>
-        </html>
-        '''
