18eb5c455da9d5419c4e77ae3a81c0e3cb542af8
thaque <thaque@thaque.com>
2019-09-11 00:28:46 +0000

    simple flask app with mysql db and bootstrap completed! yay!

--- zarin_app.py
+++ zarin_app.py
@@ -24,13 +24,15 @@ class Comment(db.Model):
     content = db.Column(db.String(4096))
 
 
-comments = []
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        return render_template("main_page.html", comments=comments)
+        return render_template("main_page.html", comments=Comment.query.all())
 
-    comments.append(request.form["contents"])
+    comment = Comment(content=request.form["contents"])
+    db.session.add(comment)
+    db.session.commit()
+
     return redirect(url_for('index'))
 
