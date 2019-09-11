fbae7cde89458d4163693dc8ec364588777d91cd
thaque <thaque@thaque.com>
2019-09-11 00:02:28 +0000

    added MySQL database

--- zarin_app.py
+++ zarin_app.py
@@ -1,7 +1,20 @@
 from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="thaque",
+    password="403295Sql",
+    hostname="thaque.mysql.pythonanywhere-services.com",
+    databasename="thaque$comments",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 comments = []
 
